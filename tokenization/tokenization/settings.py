c11c8250977fa78b196c9d9775f3199f6ab7fc29
veldakarimi <veldakarimi@gmail.com>
2022-03-09 21:49:27 +0300

    hiding secret key

--- tokenization/tokenization/settings.py
+++ tokenization/tokenization/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from secret import SECRET_KEY
+import os
+from tokenization.secret_settings import SECRET_KEY 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = SECRET_KEY 
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True 
